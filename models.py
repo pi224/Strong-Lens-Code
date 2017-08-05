ba8585b7356e03f6a9b6d16551c0d8dbf52c65b4
pgad <pgad@umich.edu>
2017-08-05 00:10:50 -0400

    Added STNs

--- models.py
+++ models.py
@@ -64,3 +64,24 @@ def simpConvNNBatchFirst(input_shape=(64, 64, 3)):
 	model.add(Dense(32, activation='softplus'))
 	model.add(Dense(1, activation='sigmoid'))
 	return model
+
+def simpConvNNSTN(input_shape=(64,64,3)):
+    model = Sequential()
+    model.add(SpatialTransformer(localization_net=locnet,
+                             downsample_factor=3, input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), input_shape=input_shape))
+	#model went from 64x64x3 to 32x32x3
+	model.add(Conv2D(64, (3, 3), strides=(2,2), activation='softplus'))
+	#model is now 16x16x64
+	model.add(Conv2D(32, (3, 3), activation='softplus'))
+	#model is now 16x16x32
+	model.add(Conv2D(16, (3, 3), activation='softplus'))
+	#model is now 16x16x16
+	model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+	#model is now  8x8x16
+	model.add(Flatten())
+	#model is now 1024 (flattened from 8x8x16)
+	model.add(Dense(128, activation='softplus'))
+	model.add(Dense(32, activation='softplus'))
+	model.add(Dense(1, activation='sigmoid'))
+	return model
