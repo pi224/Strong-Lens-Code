f4efca4c4965a7f64822ce9a7aebcb9709ab33a5
pi224 <dtzhang@umich.edu>
2017-08-26 16:35:10 -0400

    A number of changes:
    1) Adding in n-fold cross validation as a function in utils
    2) Added new options to train and test functions in utils.
    	You can now specify things like whether to print the model summary
    	And whether or not to use a validation set while training
    3) Added a models_preprocessing.py, to store the models for the
    	preprocessing subproject (this is more an organizational thing
    4) slight changes to preprocessing.py

--- models_preprocessing.py
+++ models_preprocessing.py
@@ -4,8 +4,8 @@ from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.optimizers import Adam
 
 
-def compiledConvnet():
-	model = convnet()
+def compiledConvnet(input_shape=(101, 101, 4)):
+	model = convnet(input_shape)
 	optimizer = Adam(lr = .0001, decay = 5e-5)
 	model.compile(optimizer=optimizer,
 	loss='binary_crossentropy',
@@ -17,6 +17,54 @@ def convnet(input_shape=(101, 101, 4)):
 	model = Sequential()
 	model.add(Conv2D(64, (3, 3), strides=(2,2), activation='softplus',
 					input_shape=input_shape))
+	model.add(Conv2D(32, (3, 3), strides=(2,2), activation='softplus'))
+	model.add(Conv2D(16, (3, 3), activation='softplus'))
+	model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+
+	model.add(Flatten())
+
+	model.add(Dense(128, activation='softplus'))
+	model.add(Dense(32, activation='softplus'))
+	model.add(Dense(1, activation='sigmoid'))
+	return model
+
+def compiled_maxpool_simpler_1(input_shape=(101, 101, 4)):
+	model = maxpool_simpler_1(input_shape)
+	optimizer = Adam(lr = .0001, decay = 5e-5)
+	model.compile(optimizer=optimizer,
+	loss='binary_crossentropy',
+	metrics=['accuracy'])
+
+	return model
+
+def maxpool_simpler_1(input_shape=(101, 101, 4)):
+	model = Sequential()
+	model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), input_shape=input_shape))
+	model.add(Conv2D(32, (3, 3), strides=(2,2), activation='softplus',
+					input_shape=input_shape))
+	model.add(Conv2D(16, (3, 3), strides=(2,2), activation='softplus'))
+	model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+
+	model.add(Flatten())
+
+	model.add(Dense(128, activation='softplus'))
+	model.add(Dense(32, activation='softplus'))
+	model.add(Dense(1, activation='sigmoid'))
+	return model
+
+def compiled_maxpool_convnet(input_shape=(101, 101, 4)):
+	model = convnet(input_shape)
+	optimizer = Adam(lr = .0001, decay = 5e-5)
+	model.compile(optimizer=optimizer,
+	loss='binary_crossentropy',
+	metrics=['accuracy'])
+
+	return model
+
+def maxpool_convnet(input_shape=(101, 101, 4)):
+	model = Sequential()
+	model.add(MaxPooling2D(pool_size=(4,4), strides=(4,4), input_shape=input_shape))
+	model.add(Conv2D(64, (3, 3), strides=(2,2), activation='softplus'))
 	model.add(Conv2D(32, (3, 3), strides=(2,2), activation='softplus'))
 	model.add(Conv2D(16, (3, 3), activation='softplus'))
 	model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
